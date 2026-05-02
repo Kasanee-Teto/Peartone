ae5b5cd2941c5a50f6afb36471ab348fed2fa8c5
Queen of the Underworld <jessicagunawan650@gmail.com>
2026-05-02 11:48:13 +0700

    Modified dates (createdAt, updatedAt) on migrations

--- backend/migrations/20260430171723-create_liked_tracks.cjs
+++ backend/migrations/20260430171723-create_liked_tracks.cjs
@@ -23,12 +23,12 @@ module.exports = {
       createdAt: { 
         type: Sequelize.DATE, 
         allowNull: false, 
-        defaultValue: Sequelize.NOW 
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP') 
       },
       updatedAt: { 
         type: Sequelize.DATE, 
         allowNull: false, 
-        defaultValue: Sequelize.NOW 
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP') 
       }
     });
 
