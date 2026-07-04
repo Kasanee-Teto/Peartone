5f51e5f22d71e48d336518055d77e745b3ae863f
Queen of the Underworld <jessicagunawan650@gmail.com>
2026-07-04 22:19:30 +0700

    Chore: Modify url backend into dynamic url for Google OAuth Callback

--- backend/config/passport.js
+++ backend/config/passport.js
@@ -3,12 +3,10 @@ import { Strategy as GoogleStrategy } from "passport-google-oauth20";
 
 console.log("--> Passport Config Loading. Client ID Present:", !!process.env.GOOGLE_CLIENT_ID);
 
-const PORT = process.env.PORT || 3000;
-
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,      
     clientSecret: process.env.GOOGLE_CLIENT_SECRET, 
-    callbackURL: `http://localhost:${PORT}/api/auth/google/callback`,
+    callbackURL: `${process.env.BACKEND_URL}/api/auth/google/callback`,
     proxy: true,
   },
   async (accessToken, refreshToken, profile, done) => {
